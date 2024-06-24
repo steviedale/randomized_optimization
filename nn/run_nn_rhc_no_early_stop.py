56eb47bc7e4e3ab3d3994444968eafc89b04257f
Stevie Dale <sdale4@asu.edu>
2024-06-24 08:45:38 -0700

    backup

--- nn/run_nn_rhc_no_early_stop.py
+++ nn/run_nn_rhc_no_early_stop.py
@@ -15,31 +15,30 @@ sys.path.append('../mlrose')
 from mlrose_hiive import NeuralNetwork
 
 # %%
-LR = 1e-4
-ALG = 'genetic_alg'
+LR = 1
+ALG = 'random_hill_climb'
 MAX_ATTEMPTS = 100
 CLIP = 5
 HIDDEN_NODES = [10,]
+RESTARTS = 20
 MAX_PATIENCE = 5
-POPULATION_SIZE = 200
-MUTATION_RATE = 0.01
 
 # %%
 X, y, y_one_hot = get_mnist_data()
 
 # %%
+# Split data into training and test sets
 skf = StratifiedKFold(n_splits=3, random_state=None, shuffle=True)
-run_i = 0
-train_index, test_index = list(skf.split(X, y))[run_i]
-
-start_time = time.perf_counter()
-
+train_index, test_index = list(skf.split(X, y))[0]
 X_train = X[train_index]
 X_test = X[test_index]
 y_train = y[train_index]
 y_test = y[test_index]
 y_train_one_hot = y_one_hot[train_index]
 y_test_one_hot = y_one_hot[test_index]
+
+# %%
+X_train.shape
 
 # %%
 skf = StratifiedKFold(n_splits=3, random_state=None, shuffle=True)
@@ -49,6 +48,7 @@ data = {
     'max_attempts': MAX_ATTEMPTS,
     'clip': CLIP,
     'hidden_nodes': HIDDEN_NODES,
+    'restarts': RESTARTS,
     'runs': [],
     'max_patience': MAX_PATIENCE,
 }
@@ -64,7 +64,7 @@ for run_i, (train_index, test_index) in enumerate(skf.split(X, y)):
     y_train_one_hot = y_one_hot[train_index]
     y_test_one_hot = y_one_hot[test_index]
 
-    iter_list = np.arange(1, 100001, 5) 
+    iter_list = np.arange(1, 5001, 400) 
     test_scores = []
     best_test_score = 0
     patience_counter = 0
@@ -73,31 +73,32 @@ for run_i, (train_index, test_index) in enumerate(skf.split(X, y)):
             hidden_nodes=HIDDEN_NODES, activation='relu', 
             algorithm=ALG, 
             max_iters=iterations, bias=True, is_classifier=True, 
-            learning_rate=LR, early_stopping=True, clip_max=CLIP, max_attempts=MAX_ATTEMPTS, 
-            pop_size=POPULATION_SIZE, mutation_prob=MUTATION_RATE,
-            random_state=run_i, curve=True
+            learning_rate = 1, early_stopping=False, clip_max=CLIP, max_attempts=MAX_ATTEMPTS, 
+            random_state=run_i, curve=True,
+            restarts=RESTARTS,
         )
         nn_model.fit(X_train, y_train_one_hot)
         score = nn_model.score(X_test, y_test_one_hot)
         test_scores.append(score)
 
         # early stopping
-        if score > best_test_score:
-            best_test_score = score
-            patience_counter = 0
-            print(f'New best test score: {best_test_score}')
-        else:
-            patience_counter += 1
-            print(f'Patience: {patience_counter}, Test score: {score}')
-            if patience_counter > MAX_PATIENCE:
-                print("Early stopping at ", iterations)
-                break
+        # if score > best_test_score:
+        #     best_test_score = score
+        #     patience_counter = 0
+        #     print(f'New best test score: {best_test_score}')
+        # else:
+        #     patience_counter += 1
+        #     print(f'Patience: {patience_counter}, Test score: {score}')
+        #     if patience_counter > MAX_PATIENCE:
+        #         print("Early stopping at ", iterations)
+        #         break
 
-        if len(nn_model.fitness_curve) < iterations:
-            print("Early stopping at ", iterations)
-            break
+        # if len(nn_model.fitness_curve) < iterations:
+        #     print("Early stopping at ", iterations)
+        #     break
     print(test_scores)
     end_time = time.perf_counter()
+
     run_data['test_scores'] = test_scores
     run_data['iterations'] = iter_list[:len(test_scores)]
     run_data['fitness_curve'] = nn_model.fitness_curve
@@ -105,12 +106,6 @@ for run_i, (train_index, test_index) in enumerate(skf.split(X, y)):
 
     data['runs'].append(run_data)
 
-# %%
-path = f'metrics/{ALG}/{ALG}_pop_size{POPULATION_SIZE}_mut_rate{MUTATION_RATE}_pat{MAX_PATIENCE}_lr{LR}_clip{CLIP}_max_attempts{MAX_ATTEMPTS}_hidden_nodes{HIDDEN_NODES}.pkl'
+path = f'metrics/{ALG}/{ALG}_no_early_stop_restarts{RESTARTS}_lr{LR}_clip{CLIP}_max_attempts{MAX_ATTEMPTS}_hidden_nodes{HIDDEN_NODES}.pkl'
 os.makedirs(os.path.dirname(path), exist_ok=True)
 pickle.dump(data, open(path, 'wb'))
-
-# %%
-
-
-
